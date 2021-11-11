5503f96a630105f0a1b24c7dfd2415e10a04c6e2
pantlis8 <64821829+pantlis8@users.noreply.github.com>
2021-11-10 23:37:32 -0700

    Add files via upload

---
+++ prototype/serial2/storesEncodeTemplate.py
@@ -0,0 +1,39 @@
+import serial
+import time
+
+from cryptography.fernet import Fernet
+key = b'wWGAOCPMXdL2hcgllfjwz4fu0L_2gUtDMox36UiHNBw='
+crypter = Fernet(key)
+a=[]
+b=[]
+
+def clean(L):
+    newl = []
+    for i in range(len(L)):
+        temp = L[i][2:]
+        newl.append(temp[:-6])
+    return newl
+
+
+count = 0
+ser = serial.Serial('/dev/cu.usbmodem14201', baudrate=9600, timeout=1)
+rawdata = []
+time.sleep(2)
+ser.write(b'g')
+time.sleep(2)
+i = 0
+while i < 162:
+    rawdata = str(ser.readline())
+    a.append(rawdata[2:-5])
+    i = i + 1
+
+i = 0
+while i < 162:
+    b.append(str(crypter.decrypt(str.encode(a[i])), 'utf8'))
+    i = i + 1
+
+
+i = 0
+while i < 162:
+    print(b[i])
+    i = i + 1
